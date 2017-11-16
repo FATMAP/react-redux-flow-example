766f85a8499e104931baaf04102ca66bd07abf63
Callum Macdonald <git@callum-macdonald.com>
2017-11-16 13:04:58 +0100

    Prettier.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 // @flow
 
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
+import React, { Component } from "react";
+import { Provider } from "react-redux";
 
-import configureStore from './configureStore';
+import configureStore from "./configureStore";
 
-import Counter from './Counter';
+import Counter from "./Counter";
 
-import type { Store } from './types';
+import type { Store } from "./types";
 
 const store: Store = configureStore();
 
 class App extends Component {
   render() {
     return (
-      <Provider store={ store }>
+      <Provider store={store}>
         <Counter text="some text" />
       </Provider>
     );
